d8a23307366cf28d1e565abca7d10691d1c5f172
Yunojoe <joe.ford@yunomedia.com>
2018-01-10 11:35:23 +0000

    Laving
    More Laving

---
+++ laravel/resources/views/servers.blade.php
@@ -0,0 +1,438 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Servers | Yuno Media Portal</title>
+
+  <script type="text/javascript" src="http://139.59.171.46:19999/dashboard.js"></script>
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.css" rel="stylesheet">
+     
+  </head>
+    
+   
+  <script>NETDATA.options.current.destroy_on_hide = true;</script>
+       
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <!--begin menu -->
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><img src="images/Yunowhite-01.png" style="width:75%; height: auto;"> <span>Yuno Media</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/danposh.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Dan Grover</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>Systems</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i>Finacials</a>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i>Work<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="prospects.html">Prospects</a></li>
+                      <li><a href="currentwork.html">Ongoing</a></li>
+                      <li><a href="form_validation.html">Current</a></li>
+                      <li><a href="careplans.html">Care plans</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="yunocalendar.html"><i class="fa fa-desktop"></i>Calendar</a></li>
+                  <li><a href="servers.html"><i class="fa fa-area-chart"></i>Server Status</a>
+                  </li>
+                  <li><a href="servers2.html"><i class="fa fa-gift"></i>Droplet Request</a>
+                  </li>
+                  <li><a href="servers3.html"><i class="fa fa-search"></i>Which Server</a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+              </a>
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+              </a>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+        <!-- end menu -->
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/img.jpg" alt="">John Doe
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Profile</a></li>
+                    <li>
+                      <a href="javascript:;">
+                        <span class="badge bg-red pull-right">50%</span>
+                        <span>Settings</span>
+                      </a>
+                    </li>
+                    <li><a href="javascript:;">Help</a></li>
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                  </ul>
+                </li>
+
+                <li role="presentation" class="dropdown">
+                  <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope-o"></i>
+                    <span class="badge bg-green">6</span>
+                  </a>
+                  <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a>
+                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span>
+                          <span>John Smith</span>
+                          <span class="time">3 mins ago</span>
+                        </span>
+                        <span class="message">
+                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="text-center">
+                        <a>
+                          <strong>See All Alerts</strong>
+                          <i class="fa fa-angle-right"></i>
+                        </a>
+                      </div>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>yunoweb <small>139.59.171.46</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://139.59.171.46:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>tlc <small>46.101.84.223</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://46.101.84.223:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>simplefit <small>46.101.4.20</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://46.101.4.20:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>projecthomes <small>178.62.92.110</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://178.62.92.110:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>mint <small>188.166.150.123</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://188.166.150.123:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>lizardlobby <small>188.166.156.214</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://188.166.156.214:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>growith <small>178.62.62.98</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://178.62.62.98:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>continuum <small>178.62.41.231</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://178.62.41.231:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+              <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>centaur <small>46.101.18.67</small></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content bg-dark">
+                     <div 
+                          data-netdata="system.cpu" 
+                          data-chart-library="gauge" 
+                          data-title="" 
+                          data-units="%" 
+                          data-gauge-max-value="100" 
+                          data-host="http://46.101.18.67:19999"
+                          data-after="-540" 
+                          data-points="540" 
+                          data-colors="#22AA99" 
+                          role="application">
+                  </div>
+                </div>
+              </div>
+           
+             </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+  </body>
+</html>
